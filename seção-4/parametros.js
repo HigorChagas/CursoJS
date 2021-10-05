150bbc05ef14c48336c3ef5c18d30c474e5a71af
Higor Chagas <higornchagas@gmail.com>
2021-10-05 14:26:13 -0300

    Arquivos do curso de JS

--- seção-4/parametros.js
+++ seção-4/parametros.js
@@ -1,6 +1,6 @@
 //a argumentos que sustena todos os argumentos enviados.
 // arguments não funcionam para arrow functions.
-function funcao(a, b, c) {
+/* function funcao(a, b, c) {
     let total = 0
     for (let argumento of arguments) {
         total += argumento;
@@ -9,4 +9,24 @@ function funcao(a, b, c) {
     console.log(total);
 }
 
-funcao(1, 2, 3, 4, 5, 6, 7, 8, 9, 10);
+funcao(1, 2, 3, 4, 5, 6, 7, 8, 9, 10); */
+
+/* function funcao( {nome, sobrenome, idade} ) {
+    console.log(nome, sobrenome, idade);
+}
+let obj = {nome: 'Higor', sobrenome: 'Chagas', idade: 25};
+funcao(obj); */
+
+const conta = function(operador, acumulador, ...numeros) {
+    for(let numero of numeros) {
+        if(operador === '+') acumulador += numero;
+        if(operador === '-') acumulador -= numero;
+        if(operador === '*') acumulador *= numero;
+        if(operador === '/') acumulador /= numero;
+    }
+
+    console.log(acumulador);
+};
+
+
+conta('*', 1, 20, 30, 40, 50);
