814a7aafa9e6517703004bbb5b0ad61c611f0b57
Higor Chagas <higornchagas@gmail.com>
2021-10-18 17:13:38 -0300

    Arquivos do curso de JS

---
+++ exercicios/testes.js
@@ -0,0 +1,4 @@
+const x = 6;
+const y = 6;
+
+console.log(Object.is(x, y));
--- seção-4/callback.js
+++ seção-4/callback.js
@@ -1,14 +1,39 @@
-function f1() {
-    console.log('f1');
-}
-function f2() {
-    console.log('f2');
-}
-function f3() {
-    console.log('f3');
+function rand(min = 1000, max = 3000) {
+    const numero = Math.random() * (max - min) + min;
+    return Math.floor(numero);
 }
 
-f1();
-f2();
-f3();
-console.log('Olá mundo!');
+function f1(callback) {
+    setTimeout(function() {
+        console.log('f1');
+        if(callback) callback()
+    }, rand());
+    
+}
+function f2(callback) {
+    setTimeout(function(){
+        console.log('f2');
+        if(callback) callback()
+    }, rand());
+    
+}
+function f3(callback) {
+    setTimeout(function(){
+        console.log('f3');
+        if(callback) callback();
+    }, rand())
+}
+
+f1(f1Callback);
+
+function f1Callback() {
+    f2(f2Callback);
+}
+
+function f2Callback() {
+    f3(f3Callback);
+}
+
+function f3Callback() {
+    console.log('Olá mundo!');
+}
