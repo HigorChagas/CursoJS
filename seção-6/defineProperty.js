2f3be276b7df9b2a5d56d7da73d7cdd1f35b5669
Higor Chagas <higornchagas@gmail.com>
2022-01-10 17:00:45 -0300

    Arquivos do curso de JS

--- seção-6/defineProperty.js
+++ seção-6/defineProperty.js
@@ -1,1 +1,32 @@
 // defineProperty - defineProperties
+
+function Produto(nome, preco, estoque) {
+    Object.defineProperty(this, 'estoque', {
+        enumerable: true, // Mostra a chave
+        value: estoque, // Valor da chave
+        writable: false, // Pode alterar o valor?
+        configurable: false // Pode apagar, editar ou reconfigurar a chave?
+    });
+
+    Object.defineProperties(this, {
+        nome: {
+            enumerable: true,
+            value: nome,
+            writable: true,
+            configurable: true
+        },
+        preco: {
+            enumerable: true,
+            value: preco,
+            writable: true,
+            configurable: true
+        }
+    });
+}
+
+const produtoUm = new Produto('Calça', 100, 5);
+console.log(produtoUm);
+
+for(let chave in produtoUm) {
+    console.log(chave);
+}
