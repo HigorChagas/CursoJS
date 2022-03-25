aff24719c0b0cd5b3f048f8ec58a7c250eee0b51
Higor Chagas <higornchagas@gmail.com>
2022-03-25 15:38:12 -0300

    Arquivos do curso de JS

--- seção-7/metodosEstaticos.js
+++ seção-7/metodosEstaticos.js
@@ -1,15 +1,28 @@
+function teste() {
+    console.log('Este é meu teste');
+}
+
 class ControleRemoto {
     constructor(tv) {
         this.tv = tv;
         this.volume = 0;
     }
-
+    //Método de instância
     aumentarVolume() {
         this.volume += 2;
     }
 
     diminuirVolume() {
         this.volume -= 2;
+    }
+
+    //Método estático
+    static trocaPilha() {
+        console.log('Ok, vou trocar');
+    }
+
+    static soma(x, y) {
+        console.log(this);
     }
 }
 
@@ -18,4 +31,5 @@ const controle = new ControleRemoto('Samsung');
 controle.aumentarVolume();
 controle.aumentarVolume();
 controle.aumentarVolume();
-console.log(controle);
+ControleRemoto.trocaPilha();
+ControleRemoto.soma();
--- testes.js
+++ testes.js
@@ -1,10 +1,16 @@
-function recurse(max) {
-  if(max === 10) {
-    return;
-  } 
-  max++
-  console.log(max);
-  recurse(max);
-}
+// function recurse(max) {
+//   if(max === 10) {
+//     return;
+//   } 
+//   max++
+//   console.log(max);
+//   recurse(max);
+// }
 
-recurse(2);
+// recurse(2);
+
+let a = 8, b = 6, c = 10;
+
+const numeros = [b, c ,a];
+[a, b, c] = numeros;
+console.log(numeros);
