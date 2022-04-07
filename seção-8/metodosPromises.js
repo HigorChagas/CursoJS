81a63afb93684b6a92c611f818f466c2f15cf785
Higor Chagas <higornchagas@gmail.com>
2022-04-07 17:09:53 -0300

    Arquivos do curso de JS

--- seção-8/metodosPromises.js
+++ seção-8/metodosPromises.js
@@ -17,17 +17,31 @@ function espera(msg, tempo) {
     });
 }
 
-// Promise.all Promise.race Promise.resolve Promise.reject
-const promises = [
-    espera('Promise 1', rand(1, 5)),
-    espera('Promise 2', rand(1, 5)),
-    espera('3000', rand(1, 5))
-];
+function baixaPagina() {
+    const emCache = true;
 
-Promise.race(promises)
-.then(function(valor) {
-    console.log(valor);
-})
-.catch(function(erro) {
-    console.log(erro);
-});
+    if(emCache) {
+        return Promise.resolve('Página em cache');
+    } else {
+        return espera('Baixei a página', 3000);
+    }
+}
+
+baixaPagina()
+    .then( dadosPagina => console.log(dadosPagina))
+    .catch( error => console.log('ERRO', error));
+
+// Promise.all Promise.race Promise.resolve Promise.reject
+// const promises = [
+//     espera('Promise 1', rand(1, 5)),
+//     espera('Promise 2', rand(1, 5)),
+//     espera('3000', rand(1, 5))
+// ];
+
+// Promise.race(promises)
+// .then(function(valor) {
+//     console.log(valor);
+// })
+// .catch(function(erro) {
+//     console.log(erro);
+// });
